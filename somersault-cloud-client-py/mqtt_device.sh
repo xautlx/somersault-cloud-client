91ac08aef949d4b1d207e710e6f2e749985725c7
LiXia <xautlx@hotmail.com>
2023-09-02 19:57:27 +0800

    信创国产化-东方通Tongweb适配

--- somersault-cloud-client-py/mqtt_device.sh
+++ somersault-cloud-client-py/mqtt_device.sh
@@ -12,6 +12,6 @@ cd "${SHELL_DIR}"
 echo "Startup mqtt_device.py ang log to mqtt_device.log..."
 # python3 mqtt_device.py -s \[240e:330:132:fc00:6614:bb8a:17ec:264f\]:8888 -d WUGUI01
 # python3 mqtt_device.py -s 192.168.1.200:48080 -d WUGUI01
-nohup python3 -u mqtt_device.py -s 124.71.2.242:8888 -d WUGUI01 >> mqtt_device.log 2>&1 &
+nohup python3 -u mqtt_device.py -s poc.entdiy.xyz:8888 -d WUGUI01 >> mqtt_device.log 2>&1 &
 echo "Show logs using: tail -f mqtt_device.log"
 tail -f mqtt_device.log
